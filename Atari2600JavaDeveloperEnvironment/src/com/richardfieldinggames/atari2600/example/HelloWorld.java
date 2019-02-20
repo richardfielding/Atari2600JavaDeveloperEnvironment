8ded524f3a9a25dcab868c906c213f614f01dfb4
Richard Paynter <rich@paynter.co.uk>
2019-02-20 22:18:36 +0000

    Made the HelloWorld example more interesting and uses basic features such as data, branches and zero page addresses

--- Atari2600JavaDeveloperEnvironment/src/com/richardfieldinggames/atari2600/example/HelloWorld.java
+++ Atari2600JavaDeveloperEnvironment/src/com/richardfieldinggames/atari2600/example/HelloWorld.java
@@ -2,9 +2,11 @@ package com.richardfieldinggames.atari2600.example;
 
 import com.richardfieldinggames.atari2600.developer.Atari2600Assembler;
 
+import static com.richardfieldinggames.atari2600.developer.Binary.binary;
+
 /**
  * This program demonstrates how to create a simple Atari 2600 program that fills the frame with colour
- *
+ * <p>
  * Simply run this. It will compile the program into assembler, load it into a cartridge and then
  * run that in the javatari emulator.
  */
@@ -12,19 +14,56 @@ final public class HelloWorld extends Atari2600Assembler {
   {
     cleanStart();
 
+    final int colourIndex = 128;
+    final int frameColourIndex = 129;
+
+    LDA_immediate(0);
+    STA_zeroPage(colourIndex);
+
+    final int scanLines = 192;
+
+    final String colourData = "colourData";
+
     final String startOfFrame = "startOfFrame";
     label(startOfFrame);
 
     startOfVerticalBlankCode();
+
+    // Can do some code here
+    INC_zeroPage(colourIndex);
+    LDA_zeroPage(colourIndex);
+    LSR_accumulator();
+    LSR_accumulator();
+    AND_immediate(binary("00000111"));
+    STA_zeroPage(frameColourIndex);
+
     waitForEndOfVerticalBlankCode("HelloWorld");
 
-    for (int i = 0; i < 192; i++) {
+    for (int i = 0; i < scanLines / 8; i++) {
+      LDX_zeroPage(7);
+      final String loop = "loop" + i;
+      label(loop);
+
+      LDA_zeroPage(frameColourIndex);
+      CLC();
+      ADC_immediate(1);
+      AND_immediate(binary("00000111"));
+      STA_zeroPage(frameColourIndex);
+
+      TAY();
+      LDA_absoluteY(colourData);
+
       WSYNC();
-      LDA_immediate(i);
       STA_zeroPage(COLUBK);
+      DEX();
+      BPL(loop);
     }
 
     overscanCode("HelloWorld");
+
+    // Can get colours from here:
+    // http://www.randomterrain.com/atari-2600-memories-tia-color-charts.html
+    data(colourData, new int[]{0x74, 0x78, 0x78, 0x78, 0x7C, 0x78, 0x78, 0x78});
 
     JMP_absolute(startOfFrame);
   }
